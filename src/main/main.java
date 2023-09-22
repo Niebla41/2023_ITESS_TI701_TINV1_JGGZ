5acd659ee41b6558df27a89de25ca8b9b3b1a969
niebl <niebl@DESKTOP-FV7NPNR>
2023-09-22 15:30:31 -0600

    first commit

---
+++ src/main/main.java
@@ -0,0 +1,16 @@
+/*
+* ITESS TICS
+* TALLER DE INVESTIGACION
+*INTENTO DE CURSO DE CERTIFICACION DE JAVA 0Z1-808
+*BY JGGZ
+*22/SEPTIEMBRE/2023
+*/
+package main;
+
+/**
+ *
+ * @author niebl
+ */
+public class main {
+    
+}
